4f36e4e6670d79589a53ce5ec8f72cb30bfa1ddf
Alexander Krachunov <akrachunov@gmail.com>
2026-04-11 22:02:36 +0100

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
-#[allow(unused_imports)]
 use std::env;
-#[allow(unused_imports)]
 use std::fs;
+use std::io::prelude::*;
+use flate2::read::ZlibDecoder;
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -15,6 +15,21 @@ fn main() {
         fs::create_dir(".git/refs").unwrap();
         fs::write(".git/HEAD", "ref: refs/heads/main\n").unwrap();
         println!("Initialized git directory")
+    } else if args[1] == "cat-file" && args[2] == "-p" {
+        let object_id = &args[3];
+        let path = format!(".git/objects/{}/{}", &object_id[..2], &object_id[2..]);
+        let mut d = ZlibDecoder::new(fs::File::open(path).unwrap());
+        let mut bytes = Vec::new();
+        d.read_to_end(&mut bytes).unwrap();
+        
+        // Find null byte separator - \x00 - blob 12\x00hello world
+        // blob 12 → object header
+        // \0 → null byte (not visible when printed)
+        // hello world → actual file contents
+        if let Some(null_pos) = bytes.iter().position(|&b| b == 0) {
+            let content = &bytes[null_pos + 1..];
+            print!("{}", String::from_utf8_lossy(content));
+        }
     } else {
         println!("unknown command: {}", args[1])
     }
